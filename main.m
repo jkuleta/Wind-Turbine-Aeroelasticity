573912eb4fccb5bd2d8d17baa7e6c914b11ebe9b
jkuleta <kuba.kuleta@onet.eu>
2025-05-09 11:08:59 +0200

    changes

--- main.m
+++ main.m
@@ -34,10 +34,6 @@ phi_1edge = 0.3627*blade_position.^2 + 2.5337*blade_position.^3 - 3.5772*blade_p
 dphi_1edge = 2*0.3627*radius/(R^2) + 3*2.5337*radius.^2/(R^3) - 4*3.5772*radius.^3/(R^4) + 5*2.376*radius.^4/(R^5) - 6*0.6952*radius.^5/(R^6); %first derivative
 ddphi_1edge = 2*0.3627/(R^2) + 2*3*2.5337*radius/(R^3) - 3*4*3.5772*radius.^2/(R^4) + 4*5*2.376*radius.^3/(R^5) - 5*6*0.6952*radius.^4/(R^6); %second derivative
 
-plot(radius, phi_1edge); hold on;
-plot(radius, phi_1flap);
-grid on;
-legend('edge', 'flap');
 %% Calculate masses and stiffnesses
 
 M_1flap = trapz(radius, mass_distribution.*(phi_1flap.^2));
@@ -61,26 +57,42 @@ C = [2*damping_flapwise * sqrt(K_1flap*M_1flap), 0;
 omega_1flap = sqrt(K_1flap/M_1flap);
 omega_1edge = sqrt(K_1edge/M_1edge);
 
+%% Read aerodynamic twist data and radius discretization, compute shape function
 
-% %% loop: wind speed from 3m/s to 25m/s
-% for i=1:length(v0)
-%     % call BEM, outputs: Radius, Loads, and power outputs
-%     [Rx,FN,FT,P(i)]=BEM(v0(i),omega(i),pitch(i));
-% 
-%     % plot FN and FT
-%     figure(1)
-%     plot(Rx,FN,'r-o');
-%     hold on;
-%     plot(Rx,FT,'b-o');
-%     hold on
-%     grid on
-%     xlabel('Radius(m)');
-%     ylabel('Loads(N)');
-%     legend('Fn','Ft');
-% end
-% 
-% %% plot power curve regarding wind speed
-% figure(2)
-% plot(v0,P,'b-o','linewidth',1.5);
-% xlabel('Wind speed(m/s)');
-% ylabel('Power(W)');
+twist_aero = importdata('Blade\Blade section\Blade section.dat').data(:,5);
+radius_aero = importdata('Blade\Blade section\Blade section.dat').data(:,3);
+blade_position_aero = radius_aero/R;
+
+phi_1flap_aero = 0.0622*blade_position_aero.^2 + 1.7254*blade_position_aero.^3 - 3.2452 * blade_position_aero.^4 + 4.7131*blade_position_aero.^5 - 2.2555*blade_position_aero.^6;
+phi_1edge_aero = 0.3627*blade_position_aero.^2 + 2.5337*blade_position_aero.^3 - 3.5772*blade_position_aero.^4 + 2.376*blade_position_aero.^5 - 0.6952*blade_position_aero.^6;
+
+%% Coupling with BEM - loop: wind speed from 3m/s to 25m/s
+for i=1:length(v0)
+    % call BEM, outputs: Radius, Loads, and power outputs
+    [Rx,FN,FT,P(i)]=BEM(v0(i),omega(i),pitch(i));
+
+    % translation of coordinates
+    FF = FN .* cos(pitch(i)+twist_aero) + FT .* sin(pitch(i)+twist_aero);
+    FE = FN .* -sin(pitch(i)+twist_aero) + FT .* cos(pitch(i)+twist_aero);
+
+    % calculate resulting forces
+    F_1flap = trapz(radius_aero, FF.*phi_1flap_aero);
+    F_1edge = trapz(radius_aero,FE.*phi_1edge_aero);
+
+    % plot FN and FT
+    figure(1)
+    plot(Rx,FN,'r-o');
+    hold on;
+    plot(Rx,FT,'b-o');
+    hold on
+    grid on
+    xlabel('Radius(m)');
+    ylabel('Loads(N)');
+    legend('Fn','Ft');
+end
+
+%% plot power curve regarding wind speed
+figure(2)
+plot(v0,P,'b-o','linewidth',1.5);
+xlabel('Wind speed(m/s)');
+ylabel('Power(W)');
