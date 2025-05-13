9f09005661ad3dbeb7c94a18b44cc4fabdba0af0
jkuleta <kuba.kuleta@onet.eu>
2025-05-13 13:01:12 +0200

    push pull legs
    I HATE CODING

---
+++ compute_aero_force.m
@@ -0,0 +1,17 @@
+function F_modal = compute_aero_force(x, dx, v0, omega, pitch, radius, twist, phi_1flap_aero, phi_1edge_aero)
+
+    % Convert modal deflection to blade deflection shape
+    % (You could use phi*modal amplitude here, shape already given)
+
+    [Rx,FN,FT,~] = BEM(v0, omega, pitch);  % Call your existing BEM
+
+    % Rotate loads to flap and edge directions
+    FF = FN .* cos(pitch + twist) + FT .* sin(pitch + twist);
+    FE = FN .* -sin(pitch + twist) + FT .* cos(pitch + twist);
+
+    % Project to modal coordinates
+    F_flap = trapz(radius, FF .* phi_1flap_aero);
+    F_edge = trapz(radius, FE .* phi_1edge_aero);
+
+    F_modal = [F_flap; F_edge];
+end
